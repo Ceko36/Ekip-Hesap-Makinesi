810b6e1cace46a0f07ac04632a056e15b0d8afae
hananaozbek1 <meryemozbek671@gmail.com>
2025-10-30 10:42:58 +0300

    Update calculator.py
    düzeltme

--- calculator.py
+++ calculator.py
@@ -1,7 +1,8 @@
 def calc(a, op, b):
     if op == '+': return a + b
-    if op == '-': return a - b
     if op == '*': return a * b
+    if op == '-': return a - b
+    
     if op == '/': return a / b if b != 0 else 'Sıfıra bölme hatası'
     return 'Geçersiz işlem'
 
